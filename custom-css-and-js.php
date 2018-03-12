bb71dda4e0a8ef954564f82ad1e1bc02a0558538
Dennis Kimble <info@islanderremoteservices.com>
2018-03-12 07:01:13 -0500

    First commit

---
+++ custom-css-and-js.php
@@ -0,0 +1,19 @@
+<?php
+//This is my custom php
+/*
+Plugin Name:   Dennis' Custom CSS and JS Plugin
+Plugin URI:    https://github.com/GhostInTheMachines
+Description:   Add a custom CSS stylesheet and JavaScript file to WordPress
+Version:       1.0
+Author:        Dennis Kimble
+Author URI:    http://islanderremoteservices.com/web-services
+*/
+function custom_css_js_enqueue_scripts() {
+
+    /* enqueue the custom-style.css file */
+    wp_enqueue_style( 'custom-css', plugins_url( '/css/custom-style.css', __FILE__ ), $ver = false );
+
+    /* enqueue the custom-scripts.js file */
+    wp_enqueue_script( 'custom-js', plugins_url( '/js/custom-scripts.js', __FILE__ ), $deps = array( 'jquery' ), $ver = false, $in_footer = true );
+}
+add_action( 'wp_enqueue_scripts', 'custom_css_js_enqueue_scripts' );
---
+++ js/custom-scripts.js
@@ -0,0 +1,10 @@
+/*
+*   This is my custom javascript
+*
+ */
+jQuery( "h2.entry-title" ).each( function() {
+    var panelId = jQuery( this ).html().toLowerCase().replace(/\s+/g, "-");
+    jQuery( this ).wrapInner(function() {
+        return "<span style='padding-top:96px;' id='" + panelId + "'></span>";
+    })
+})
